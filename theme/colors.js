2e74a6fa08a7ba8d42cb660f0d58e0de93867ef0
Osagiede Harrison <backendharry@gmail.com>
2023-11-07 10:45:17 +0100

    added theme setup

---
+++ theme/colors.js
@@ -0,0 +1,29 @@
+const COLORS = {
+  primary: "#015246",
+
+  background: "#010101",
+
+  bluePrimary: "#51a6fc",
+
+  white: "#ffffff",
+
+  searchGrayBackground: "#2a2a2c",
+  searchGrayPlaceholder: "#9f9ea5",
+
+  chatWhite: "#fdfdfd",
+  chatGray: "#8f8e95",
+
+  grayIcon: "#7e7d84",
+  navBottomGradient1: "#1c1c1b",
+  navBottomGradient2: "#161415",
+
+  grayTime: " #8caeae",
+
+  doubleCheckIcon: "#2fa1fe",
+
+  replyBackground: "#363638",
+
+  messageInputBackground: "#2c2c2e",
+};
+
+export default COLORS;
---
+++ theme/fonts.js
@@ -0,0 +1,8 @@
+const FONTS = {
+  light: "HNLight",
+  medium: "HNMediuim",
+  bold: "HNBold",
+  black: "HNBlack",
+};
+
+export default FONTS;
