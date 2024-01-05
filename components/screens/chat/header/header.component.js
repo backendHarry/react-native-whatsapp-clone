c918647df7481a32ebffbcf6f7d27a71d601cea1
Osagiede Harrison <backendharry@gmail.com>
2024-01-05 17:51:12 +0100

    setting router to help go back in history from chat

--- components/screens/chat/header/header.component.js
+++ components/screens/chat/header/header.component.js
@@ -1,7 +1,8 @@
-import { View, Image } from "react-native";
+import { View, Image, TouchableWithoutFeedback } from "react-native";
 import Text from "../../../config/Text";
 import styles from "./header.style";
 import { BlurView } from "expo-blur";
+import { useRouter } from "expo-router";
 
 import { LeftIcon, VideoCall, CallIcon } from "../../../../assets/icons/index";
 import { COLORS, CONSTANTS } from "../../../../theme";
@@ -9,19 +10,26 @@ import { COLORS, CONSTANTS } from "../../../../theme";
 import TestImage from "../../../../assets/images/profile.jpeg";
 
 const Header = () => {
+  const router = useRouter();
+
   return (
-    <BlurView intensity={40} style={styles.chatHeader}>
+    <BlurView intensity={80} style={styles.chatHeader}>
       <View style={styles.chatHeaderContainer}>
         <View style={styles.chatHeaderLeft}>
-          <View style={styles.leftIcon}>
-            <LeftIcon
-              stroke={COLORS.bluePrimary}
-              height={CONSTANTS.iconHeight + 10}
-              width={CONSTANTS.iconWidth + 10}
-              strokeWidth={1.5}
-            />
-            <Text style={styles.numberOfMessages}>7</Text>
-          </View>
+          <TouchableWithoutFeedback
+            style={styles.leftIcon}
+            onPress={() => router.back()}
+          >
+            <View style={{ flexDirection: "row", alignItems: "center" }}>
+              <LeftIcon
+                stroke={COLORS.bluePrimary}
+                height={CONSTANTS.iconHeight + 10}
+                width={CONSTANTS.iconWidth + 20}
+                strokeWidth={1.5}
+              />
+              <Text style={styles.numberOfMessages}>7</Text>
+            </View>
+          </TouchableWithoutFeedback>
           <View style={styles.profileImageAndName}>
             <View style={styles.profileImageContainer}>
               <Image source={TestImage} style={styles.profileImage} />
